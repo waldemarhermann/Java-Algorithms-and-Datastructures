6fe171910c080500570eea8830604c004bc9732c
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-07-20 18:14:17 +0200

    update LinkedList and Main to latest version

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,249 @@
+
 public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+    }
+
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void showList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length ++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+
+        tail = pre;
+        tail.next = null;
+        length--;
+
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length ++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length --;
+
+        if (length == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node get(int index) {
+        Node temp = head;
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index == 0) {
+            prepend(value);
+            return true;
+        } else if (index == length) {
+            append(value);
+            return true;
+        } else {
+            Node newNode = new Node(value);
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+            length ++;
+            return true;
+        }
+
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+
+        /*Node pre = head;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            pre = temp;
+            temp = temp.next;
+        }*/
+
+        Node pre = get(index - 1);
+        Node temp = pre.next;
+
+        pre.next = temp.next;
+        temp.next = null;
+        length --;
+        return temp;
+
+    }
+
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+
+
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+
+        }
+    }
+
+    public boolean hasLoop() {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public void partitionList(int pivot) {
+        Node smallerDummy = new Node(0);
+        Node greaterDummy = new Node(0);
+        Node smaller = smallerDummy;
+        Node greater = greaterDummy;
+        Node current = head;
+
+        while(current != null) {
+            if(current.value < pivot) {
+                smaller.next = current;
+                smaller = current;
+            } else {
+                greater.next = current;
+                greater = current;
+            }
+            current = current.next;
+        }
+        smaller.next = greaterDummy.next;
+        greater.next = null;
+        head = smallerDummy.next;
+    }
+
+    public void removeDuplicates() {
+        Node current = head;
+
+        while(current != null && current.next != null) {
+            Node runner = current;
+
+            while(runner.next != null) {
+                if(runner.next.value == current.value) {
+                    runner.next = runner.next.next;
+                    length--;
+                    if(runner.next == null) {
+                        tail = runner;
+                    }
+                } else {
+                    runner = runner.next;
+                }
+            }
+            current = current.next;
+        }
+    }
+
+    public int binaryToDecimal() {
+        Node temp = head;
+        int num = 0;
+
+        while(temp != null) {
+            num = num * 2 + temp.value;
+            temp = temp.next;
+        }
+        return num;
+    }
+
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // ---------------
+        // Convert 1011 to 11
+        // ---------------
+        LinkedList list1 = new LinkedList(1);
+        list1.append(0);
+        list1.append(1);
+        list1.append(1);
+        System.out.println("Convert 1011 to 11:");
+        System.out.println("Input: 1 -> 0 -> 1 -> 1");
+        System.out.println("Output: " + list1.binaryToDecimal());
+        System.out.println("---------------");
+
+        // ---------------
+        // Convert 1100 to 12
+        // ---------------
+        LinkedList list2 = new LinkedList(1);
+        list2.append(1);
+        list2.append(0);
+        list2.append(0);
+        System.out.println("Convert 1100 to 12:");
+        System.out.println("Input: 1 -> 1 -> 0 -> 0");
+        System.out.println("Output: " + list2.binaryToDecimal());
+        System.out.println("---------------");
+
+        // ---------------
+        // Convert 1 to 1
+        // ---------------
+        LinkedList list3 = new LinkedList(1);
+        System.out.println("Convert 1 to 1:");
+        System.out.println("Input: 1");
+        System.out.println("Output: " + list3.binaryToDecimal());
+        System.out.println("---------------");
+
+        // ---------------
+        // Convert empty list to 0
+        // ---------------
+        LinkedList list4 = new LinkedList(0);
+        list4.makeEmpty();
+        System.out.println("Convert empty list to 0:");
+        System.out.println("Input: empty");
+        System.out.println("Output: " + list4.binaryToDecimal());
+        System.out.println("---------------");
     }
 }
