cc9b788d4e2837cfadcd471a76ff0d9cfbf720b9
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-07-21 10:28:03 +0200

    add class DoublyLinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        // Create a linked list with values 1, 2, 3, 4, and 5
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(2);
-        myLinkedList.append(3);
-        myLinkedList.append(4);
-        myLinkedList.append(5);
 
-        System.out.println("Original linked list: ");
-        myLinkedList.printList();
+        DoublyLinkedList myDLL = new DoublyLinkedList(4);
 
-        // Reverse a sublist within the linked list
-        myLinkedList.reverseBetween(1, 3);
-        System.out.println("\nReversed sublist (1, 3): ");
-        myLinkedList.printList();
-
-        // Reverse another sublist within the linked list
-        myLinkedList.reverseBetween(0, 4);
-        System.out.println("\nReversed entire linked list: ");
-        myLinkedList.printList();
-
-        // Reverse a sublist of length 1 within the linked list
-        myLinkedList.reverseBetween(2, 2);
-        System.out.println("\nReversed sublist of length 1 (2, 2): ");
-        myLinkedList.printList();
-
-        // Reverse an empty linked list
-        LinkedList emptyList = new LinkedList(0);
-        emptyList.makeEmpty();
-        emptyList.reverseBetween(0, 0);
-        System.out.println("\nReversed empty linked list: ");
-        emptyList.printList();
-
-        /*
-            EXPECTED OUTPUT:
-            ----------------
-            Original linked list:
-            1
-            2
-            3
-            4
-            5
-
-            Reversed sublist (1, 3):
-            1
-            4
-            3
-            2
-            5
-
-            Reversed entire linked list:
-            5
-            2
-            3
-            4
-            1
-
-            Reversed sublist of length 1 (2, 2):
-            5
-            2
-            3
-            4
-            1
-
-            Reversed empty linked list:
-
-        */
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+        myDLL.printList();
     }
 }
