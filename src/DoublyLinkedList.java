ab25f74d05e7e2f36de934e5d941c6a5cb27aa4c
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-07-22 20:35:16 +0200

    add new methods

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -7,7 +7,7 @@ public class DoublyLinkedList {
     class Node {
         int value;
         Node next;
-        Node pre;
+        Node prev;
 
         Node(int value) {
             this.value = value;
@@ -39,6 +39,82 @@ public class DoublyLinkedList {
 
     public void getLength() {
         System.out.println("Length: " + length);
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+            length++;
+        }
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        if(length == 1) {
+            Node temp = head;
+            head = null;
+            tail = null;
+            length--;
+            return temp;
+        } else {
+            Node temp = tail.prev;
+            tail.prev = null;
+            temp.next = null;
+            length--;
+            return temp;
+        }
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            head.prev = newNode;
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (length == 0) return null;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(4);
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
 
-        myDLL.getHead();
-        myDLL.getTail();
-        myDLL.getLength();
+        myDLL.append(2); // 1
+        myDLL.append(3); // 2
+        myDLL.append(4); // 3
+        myDLL.append(5); // 4
+
+        System.out.println(myDLL.set(2, 9));
         myDLL.printList();
+
     }
 }
