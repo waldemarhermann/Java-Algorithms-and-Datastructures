2cebcf566d6cf5b36afe81c862a195080d74dbaf
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-07-23 15:40:22 +0200

    method reverse

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -161,4 +161,19 @@ public class DoublyLinkedList {
         tail.value = temp;
     }
 
+    public void reverse() {
+        Node current = head;
+        Node temp = current.prev;
+
+        while(current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+        temp = head;
+        head = tail;
+        tail = temp;
+    }
+
 }
