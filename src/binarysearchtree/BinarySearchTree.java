18a290d74a97e1f1e285595779a86ea07662f9eb
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-08-04 21:48:20 +0200

    add contains method in BST

--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -42,4 +42,19 @@ public class BinarySearchTree {
         }
     }
 
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node temp = root;
+        while (temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/binarysearchtree/Main.java
+++ src/binarysearchtree/Main.java
@@ -4,33 +4,37 @@ public class Main {
 
     public static void main(String [] args) {
         BinarySearchTree myBST = new BinarySearchTree();
+
         myBST.insert(2);
         myBST.insert(1);
         myBST.insert(3);
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
 
-        /*
-            THE LINES ABOVE CREATE THIS TREE:
-                         2
-                        / \
-                       1   3
-        */
 
+        System.out.println("BST Contains 27:");
+        System.out.println(myBST.contains(27));
 
-        System.out.println("Root: " + myBST.getRoot().value);
-        System.out.println("\nRoot->Left: " + myBST.getRoot().left.value);
-        System.out.println("\nRoot->Right: " + myBST.getRoot().right.value);
+        System.out.println("\nBST Contains 17:");
+        System.out.println(myBST.contains(17));
 
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            Root: 2
+            BST Contains 27:
+            true
 
-            Root->Left: 1
-
-            Root->Right: 3
+            BST Contains 17:
+            false
 
         */
+
 
     }
 }
