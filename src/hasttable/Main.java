7fb8ab45a9a23e9660de82666cb3681d6106246a
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-08-05 14:36:11 +0200

    add HashTable class

---
+++ src/hasttable/Main.java
@@ -0,0 +1,9 @@
+package hasttable;
+
+public class Main {
+
+    public static void main(String [] args) {
+        HashTable myHashTable = new HashTable();
+        myHashTable.printTable();
+    }
+}
