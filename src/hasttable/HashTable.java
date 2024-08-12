3d28d72cb7ec4c54a95b229f636942e6572089c4
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-08-12 19:48:10 +0200

    add method findDuplicates

--- src/hasttable/HashTable.java
+++ src/hasttable/HashTable.java
@@ -1,6 +1,9 @@
 package hasttable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HashTable {
 
@@ -85,5 +88,22 @@ public class HashTable {
             }
         }
         return allKeys;
+    }
+
+    public List<Integer> findDuplicates(int[] nums) {
+        HashMap<Integer, Integer> myHashMap = new HashMap<>();
+
+        for (int num : nums) {
+            myHashMap.put(num, myHashMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> duplicates = new ArrayList<>();
+
+        for (Map.Entry<Integer, Integer> entry : myHashMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                duplicates.add(entry.getKey());
+            }
+        }
+        return duplicates;
     }
 }
--- src/hasttable/Main.java
+++ src/hasttable/Main.java
@@ -1,9 +1,12 @@
 package hasttable;
+import java.util.List;
 
 public class Main {
 
     public static void main(String [] args) {
+        int[] nums = {1, 2, 3, 2, 1, 4, 5, 4};
         HashTable myHashTable = new HashTable();
-        myHashTable.printTable();
+        List duplicates = myHashTable.findDuplicates(nums);
+        System.out.println(duplicates);
     }
 }
