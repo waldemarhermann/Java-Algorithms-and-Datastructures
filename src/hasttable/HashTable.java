b2c97236548fc4ef056b37e554798de310626308
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-08-13 22:09:16 +0200

    add method twoSum

--- src/hasttable/HashTable.java
+++ src/hasttable/HashTable.java
@@ -122,4 +122,19 @@ public class HashTable {
         }
         return null;
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int complement = target - num;
+
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(num, i);
+        }
+        return new int[]{};
+    }
 }
--- src/hasttable/Main.java
+++ src/hasttable/Main.java
@@ -1,14 +1,18 @@
 package hasttable;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String [] args) {
 
         HashTable myHashTable = new HashTable();
+        System.out.println(Arrays.toString(myHashTable.twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(myHashTable.twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(myHashTable.twoSum(new int[]{3, 3}, 6)));
+        System.out.println(Arrays.toString(myHashTable.twoSum(new int[]{1, 2, 3, 4, 5}, 10)));
+        System.out.println(Arrays.toString(myHashTable.twoSum(new int[]{1, 2, 3, 4, 5}, 7)));
+        System.out.println(Arrays.toString(myHashTable.twoSum(new int[]{1, 2, 3, 4, 5}, 3)));
+        System.out.println(Arrays.toString(myHashTable.twoSum(new int[]{}, 0)));
 
-        System.out.println(myHashTable.firstNonRepeatingChar("leetcode"));
-        System.out.println(myHashTable.firstNonRepeatingChar("hello"));
-        System.out.println(myHashTable.firstNonRepeatingChar("aabbcc"));
     }
 }
