42b4ced0b9270ff073091a676cde66e3153dd22a
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-08-15 18:55:21 +0200

    add method findPairs

--- src/hasttable/HashTable.java
+++ src/hasttable/HashTable.java
@@ -191,4 +191,21 @@ public class HashTable {
         }
         return true;
     }
+
+    public List<int[]> findPairs(int[] arr1, int[] arr2, int target) {
+        Set<Integer> mySet = new HashSet<>();
+        List<int[]> pairs = new ArrayList<>();
+
+        for (int num : arr1) {
+            mySet.add(num);
+        }
+
+        for (int num : arr2) {
+            int complement = target - num;
+            if (mySet.contains(complement)) {
+                pairs.add(new int[]{complement, num} );
+            }
+        }
+        return pairs;
+    }
 }
