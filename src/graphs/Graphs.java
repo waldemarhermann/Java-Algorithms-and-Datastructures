4105d5400517340279bae98a45bec45aa742f1c1
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-08-22 19:44:09 +0200

    add method addEdge

--- src/graphs/Graphs.java
+++ src/graphs/Graphs.java
@@ -16,4 +16,13 @@ public class Graphs {
         }
         return false;
     }
+
+    public boolean addEdge(String vortex1, String vortex2) {
+        if (adjList.get(vortex1) != null && adjList.get(vortex2) != null) {
+            adjList.get(vortex1).add(vortex2);
+            adjList.get(vortex2).add(vortex1);
+            return true;
+        }
+        return false;
+    }
 }
--- src/graphs/Main.java
+++ src/graphs/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Graphs myGraph = new Graphs();
         myGraph.addVortex("A");
+        myGraph.addVortex("B");
+        myGraph.printGraph();
+
+        myGraph.addEdge("A", "B");
         myGraph.printGraph();
     }
 }
