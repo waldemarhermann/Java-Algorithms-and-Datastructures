e9bdfb3600cccf2df4e565bb0b201d2e5dafa333
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-08-23 21:11:47 +0200

    add new DS Heap

--- src/graphs/Main.java
+++ src/graphs/Main.java
@@ -7,12 +7,16 @@ public class Main {
         myGraph.addVortex("A");
         myGraph.addVortex("B");
         myGraph.addVortex("C");
+        myGraph.addVortex("D");
 
         myGraph.addEdge("A", "B");
         myGraph.addEdge("A", "C");
         myGraph.addEdge("B", "C");
+        myGraph.addEdge("A", "D");
+        myGraph.addEdge("B", "D");
+        myGraph.addEdge("C", "D");
 
-        myGraph.removeEdge("A", "B");
+        myGraph.removeVertex("D");
         myGraph.printGraph();
     }
 }
---
+++ src/heap/Heap.java
@@ -0,0 +1,34 @@
+package heap;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Heap {
+
+    private List<Integer> heap;
+
+    public Heap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+
+    public int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    public int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    public int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    public void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+}
